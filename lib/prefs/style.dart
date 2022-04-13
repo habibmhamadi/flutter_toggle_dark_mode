dd904a4a0c2a98bfa1e77dc9b5cd4924b965f5fd
kingkonglong <44733546+kingkonglong@users.noreply.github.com>
2022-04-13 09:47:06 +0200

    Fix problems

--- lib/prefs/style.dart
+++ lib/prefs/style.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 class Style {
   static ThemeData themeData(bool isDarkTheme) {
     return ThemeData(
-      primaryColor: isDarkTheme ? Colors.black : Colors.blue,
+      primaryColor: isDarkTheme ? Colors.black : Colors.deepPurple,
       scaffoldBackgroundColor: isDarkTheme ? Color(0xFF222222) : Colors.white,
     );
   }
